aabee29cd7f596fb1e86e40fc60d59ad1f67e959
Molin Deng <zjumorning@gmail.com>
2018-08-13 09:54:02 +0800

    1114 23/25

--- 1114.cpp
+++ 1114.cpp
@@ -5,49 +5,46 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <set>
 
 using namespace std;
 
 struct Person {
     int pre;
     int minID;
-    int totM;
-    double totArea;
+    int Me;
+    double Area;
     Person() {
         pre = -1;
         minID = 10000;
-        totM = totArea = 0;
+        Me = Area = 0;
     }
 };
 vector<Person> people(10010);
-vector<bool> inCount(10010, false);
+
 int rfind(int id) {
     if(people[id].pre < 0) return id;
     return people[id].pre = rfind(people[id].pre);
 }
 
-void _union(int a, int b) {
+void Union(int a, int b) {
     int r1 = rfind(a);
     int r2 = rfind(b);
-    if(r1 == r2) {
-        people[r1].minID = min(people[r1].minID, min(a, b));
-    }
-    else {
+    if(r1 != r2) {
         people[r1].pre += people[r2].pre;
         people[r2].pre = r1;
-        people[r1].minID = min(min(people[r1].minID, people[r2].minID), min(a, b));
     }
 }
 bool cmp(int a, int b) {
-    int avgArea_a = people[a].totArea / people[a].pre * (-1);
-    int avgArea_b = people[b].totArea / people[b].pre * (-1);
-    if(avgArea_a != avgArea_b) return avgArea_a > avgArea_b;
+    double avg_area_a = (double)people[a].Area / people[a].pre * (-1);
+    double avg_area_b = (double)people[b].Area / people[b].pre * (-1);
+    if(avg_area_a != avg_area_b) return avg_area_a > avg_area_b;
     else return a < b;
 }
 int main() {
     int N;
-    vector<int> res, ids;
     scanf("%d", &N);
+    set<int> ids;
     while(N--) {
         int id, fid, mid, k, Me;
         double Area;
@@ -60,27 +57,39 @@ int main() {
         }
         scanf("%d %lf", &Me, &Area);
         people[id].minID = id;
-        people[id].totM = Me;
-        people[id].totArea = Area;
-        if(fid != -1) _union(id, fid);
-        if(mid != -1) _union(id, mid);
-        if(!cids.empty()) for(auto cid : cids) _union(id, cid);
-        ids.push_back(id);
-    }
-    for(auto id : ids) {
-        int root = rfind(id);
-        if(id != root) {
-            people[root].totM += people[id].totM;
-            people[root].totArea += people[id].totArea;
+        people[id].Me = Me;
+        people[id].Area = Area;
+        ids.insert(id);
+        if(fid != -1) {
+            Union(id, fid);
+            ids.insert(fid);
         }
-        if(!inCount[root]) {
+        if(mid != -1) {
+            Union(id, mid);
+            ids.insert(mid);
+        }
+        for(auto cid : cids) {
+            Union(id, cid);
+            ids.insert(cid);
+        }
+    }
+    vector<int> res;
+    vector<bool> inCounted(10010, false);
+    for(auto iter = ids.begin(); iter != ids.end(); iter++) {
+        int root = rfind(*iter);
+        if(!inCounted[root]) {
             res.push_back(root);
-            inCount[root] = true;
+            inCounted[root] = true;
         }
+        if(root != *iter) {
+            people[root].Me += people[*iter].Me;
+            people[root].Area += people[*iter].Area;
+        }
+        people[root].minID = min(people[root].minID, *iter);
     }
-    printf("%lld\n", res.size());
     sort(res.begin(), res.end(), cmp);
-    for(auto p : res) 
-        printf("%04d %d %.3f %.3f\n", people[p].minID, people[p].pre*(-1), people[p].totM / (double)(people[p].pre*(-1)), people[p].totArea / (double)(people[p].pre*(-1)));
+    printf("%lld\n", res.size());
+    for(auto id : res) 
+        printf("%04d %d %.3lf %.3lf\n", people[id].minID, people[id].pre*(-1), (double)people[id].Me/people[id].pre*(-1), (double)people[id].Area/people[id].pre*(-1));
     return 0;
 }   
