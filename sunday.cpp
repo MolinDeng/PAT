dc30aafc5eb965e3141c285598dbb6c74b3f74bd
Molin Deng <zjumorning@gmail.com>
2018-09-27 20:07:18 +0800

    Sunday algorithm for substring match

---
+++ sunday.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> shift;
+
+void get_shift(string& P) {
+    int pLen = P.length();
+    shift.resize(256, pLen+1);
+    for(int i = 0; i < pLen; ++i) 
+        shift[P[i]] -= i+1; 
+}
+
+void Sunday(string& S, string& P) {
+    get_shift(P);
+    int sLen = S.length(), pLen = P.length();
+    for(int i = 0, j = 0; i + pLen <= sLen; i += shift[S[i + pLen]], j = 0) {
+        while(j < pLen && S[i+j] == P[j]) j++;
+        if(j == pLen) {
+            cout << "Pattern starts with idx: " << i << endl;
+            return;
+        }
+    }
+    cout << "Not Found" << endl;
+}
+int main(int argc, char const *argv[])
+{
+    string S = "search substring";
+    string P = "substr";
+    Sunday(S, P);
+    return 0;
+}
