8467fa07918f28aec3eff82c8e9bf81340e7f8ec
Molin Deng <zjumorning@gmail.com>
2018-10-13 21:01:39 +0800

    fix error

--- sunday.cpp
+++ sunday.cpp
@@ -9,14 +9,16 @@ vector<int> shift;
 void get_shift(string& P) {
     int pLen = P.length();
     shift.resize(256, pLen+1);
-    for(int i = 0; i < pLen; ++i) 
-        shift[P[i]] -= i+1; 
+    for(int i = 0; i < pLen; ++i) { 
+        shift[P[i]] = pLen - i; 
+        cout << P[i] << ": " << shift[P[i]] << endl;
+    }
 }
 
 void Sunday(string& S, string& P) {
     get_shift(P);
     int sLen = S.length(), pLen = P.length();
-    for(int i = 0, j = 0; i + pLen <= sLen; i += shift[S[i + pLen]], j = 0) {
+    for(int i = 0, j = 0; i + pLen < sLen; i += shift[S[i + pLen]], j = 0) {
         while(j < pLen && S[i+j] == P[j]) j++;
         if(j == pLen) {
             cout << "Pattern starts with idx: " << i << endl;
